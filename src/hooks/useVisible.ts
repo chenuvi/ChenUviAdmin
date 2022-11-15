6b95fb9eb8a61abf26d0edf348ca973f4a4e48ff
kun.chen <kun.chen@yintech.cn>
2022-11-15 15:57:27 +0800

    Feat: useVisible

---
+++ src/hooks/useVisible.ts
@@ -0,0 +1,16 @@
+﻿import { ref } from "vue";
+
+export default function useVisible(initValue = true) {
+  const visible = ref(initValue);
+  const setVisible = (value: boolean) => {
+    visible.value = value;
+  };
+  const toggleVisible = () => {
+    visible.value = !visible.value;
+  };
+  return {
+    visible,
+    setVisible,
+    toggleVisible,
+  };
+}
